8093114a49525d24e1d850460302ddf711dccf3a
Dhwani26 <dhwaniagarwal.97@gmail.com>
2020-10-06 21:23:49 +0530

    Implemented OnCreateViewHolder

--- app/src/main/java/com/example/androiddevworkshop/CustomAdapter.java
+++ app/src/main/java/com/example/androiddevworkshop/CustomAdapter.java
@@ -2,8 +2,11 @@ package com.example.androiddevworkshop;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder>{
     /* This is the data that the adapter will work upon*/
@@ -22,7 +25,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
     @NonNull
     @Override
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        /* LayoutInflater is used to inflate(create) views to be displayed onto the screen */
+        LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
+
+        /* Inflate the view */
+        View view = (View) layoutInflater.inflate(R.layout.custom_list_item, viewGroup, false);
+
+        /* Return the newly created ViewHolder */
+        return new CustomViewHolder(view);
     }
 
     /*
@@ -41,10 +51,21 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         return data.length;
     }
 
+    /*
+    * Holds all the views along with some references so as to avoid calling findViewById again and again
+    */
     public class CustomViewHolder extends RecyclerView.ViewHolder{
+
+        /* References */
+        ImageView imageView ;
+        TextView textView ;
 
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            /* Assign the references */
+            imageView = itemView.findViewById(R.id.itemImage);
+            textView = itemView.findViewById(R.id.itemText);
         }
     }
 }
